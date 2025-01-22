86112368c762be0210025368006d885ad8fba276
Gebrecherkos Hagos <GHagos@Mac.attwifi.manager>
2025-01-22 01:03:46 -0500

    app change

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,13 +1,14 @@
-import { StyleSheet,View,Text } from "react-native";
+import { StyleSheet,View,Text, Alert } from "react-native";
 const Heading=(props)=>{
     const {text}=props;
     return(
+        
         <Text style ={styles.text}>{text}</Text>
     )
 }
 const styles=StyleSheet.create({
     text:{
-        fontSize:24,
+        fontSize:25,
         fontWeight:'bold',
         alignSelf:'center',
         width:'100%'
